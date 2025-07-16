ed71b86895d91210aa34460764958366f2fb8037
yatish7 <yatish.manne09@gmail.com>
2025-07-16 21:53:24 +0530

    For intial deploy

--- app.py
+++ app.py
@@ -21,4 +21,5 @@ def dashboard():
     return render_template("dashboard.html", alerts=alerts)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
